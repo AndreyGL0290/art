6454868317a27288c5026c7d3d0b3764f5692cb8
Andrew Glukhov <andriy.glukhov@yandex.rucd>
2021-05-04 17:49:55 +0700

    Added save func, fixed canvas bg

--- script.js
+++ script.js
@@ -4,11 +4,23 @@ let context = canvas.getContext("2d");
 let color = "black";
 let brush_width = document.getElementById("marker_width").value;
 let style = "Marker";
+let correctarray = document.getElementsByClassName('correction');
+
+// Корректируем положение объектов на странице
+correctarray[0].height = document.getElementById('eraser').height;
+correctarray[0].width = document.getElementById('eraser').width;
+
+correctarray[1].height = document.getElementById('clear_button').height;
+correctarray[1].width = document.getElementById('clear_button').width;
 
 canvas.width = canvas.clientWidth;
 canvas.height = canvas.clientHeight;
 
-//Добавляем функцию рисования
+// Задаем задний фон непрозрачный на сохраненной картинке
+context.fillStyle = bg.value;
+context.fillRect(0, 0, canvas.width, canvas.height);
+
+// Добавляем функцию рисования на компьютере
 canvas.onmousedown = (e) => {
     // Выбираем режим рисования
     style = document.getElementById("eraser").textContent;
@@ -16,7 +28,6 @@ canvas.onmousedown = (e) => {
     color = document.getElementById("color").value;
     // Выбираем толщину маркера
     brush_width = document.getElementById("marker_width").value;
-    setTimeout(() => {
         canvas.onmousemove = (event) => {
             // Режим ластика
             if (style == "Eraser"){
@@ -28,7 +39,6 @@ canvas.onmousedown = (e) => {
                 context.clearRect(event.offsetX-brush_width/2, event.offsetY-brush_width/2, brush_width, brush_width);
             }
         };
-    }, 1);
     // Режим ластика
     if (style == "Eraser"){
         context.fillStyle = color;
@@ -42,9 +52,12 @@ canvas.onmousedown = (e) => {
         canvas.onmousemove = null;
     };
 };
+
 // Отчистка холста
 function clear(){
-    context.clearRect(0,0,canvas.width, canvas.height);
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = bg.value;
+    context.fillRect(0, 0, canvas.width, canvas.height);
 }
 // Меняем режим рисования для понимания пользователя
 function eraser(){
@@ -54,19 +67,25 @@ function eraser(){
     else{
         style = document.getElementById("eraser").textContent = "Eraser";
     }
-
 }
 
+// Делаем плавное изменение цвета
 bg.addEventListener('input', bg_color, false);
 
-// Меняем задний фон холста
 function bg_color(){
-    document.getElementById("canvas").style.backgroundColor = document.getElementById("background_color").value;
-};
+    context.fillStyle = bg.value;
+    context.fillRect(0, 0, canvas.width, canvas.height);
+}
 
 // Корректируем значение толщины маркера если оно неположительное
 function width_marker_correction(){
     if (document.getElementById("marker_width").value <= 0){
         document.getElementById("marker_width").value = -brush_width;
     }
+}
+
+// Сохраняем рисунок
+document.getElementById("save_button").onclick = function(){
+    let image = canvas.toDataURL("image/jpg");
+    this.href = image;
 }
