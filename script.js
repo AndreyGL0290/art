257bd80c97a4bceba1f906d2d97edbaac6a62cc6
Andrew Glukhov <andriy.glukhov@yandex.rucd>
2021-05-30 15:16:49 +0700

    Update save function

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 let display = 0;
+let canvas = document.getElementById("canvas");
+let canvas_bg = document.getElementById("canvas_bg");
+let bg = document.getElementById('background_color');
+let context = canvas.getContext("2d");
+let context_bg = canvas_bg.getContext("2d");
+let color = "black";
+let brush_width = document.getElementById("marker_width").value;
+let mode = "Marker";
+let correctarray = document.getElementsByClassName('correction');
+
+// Определяем девайс с которого открыт сайт
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
     display = 'Mobile';
 } else {
     display = 'Computer';
 }
-let canvas = document.getElementById("canvas");
-let bg = document.getElementById('background_color');
-let context = canvas.getContext("2d");
-let color = "black";
-let brush_width = document.getElementById("marker_width").value;
-let mode = "Marker";
-let correctarray = document.getElementsByClassName('correction');
 
 // Корректируем положение объектов на странице
 correctarray[0].height = document.getElementById('eraser').height;
@@ -19,13 +23,16 @@ correctarray[0].width = document.getElementById('eraser').width;
 correctarray[1].height = document.getElementById('clear_button').height;
 correctarray[1].width = document.getElementById('clear_button').width;
 
-// Корректируем размеры холста
+// Корректируем размеры холстов
 canvas.width = canvas.clientWidth;
 canvas.height = canvas.clientHeight;
 
-// Задаем задний фон непрозрачный на сохраненной картинке
-context.fillStyle = bg.value;
-context.fillRect(0, 0, canvas.width, canvas.height);
+canvas_bg.width = canvas_bg.clientWidth;
+canvas_bg.height = canvas_bg.clientHeight;
+
+// Устанавливаем цет фона
+context_bg.fillStyle = bg.value;
+context_bg.fillRect(0, 0, canvas_bg.width, canvas_bg.height);
 
 if (display == 'Computer') {
     // Добавляем функцию рисования на компьютере
@@ -44,8 +51,7 @@ if (display == 'Computer') {
             }
             // Режим ластика
             else if (mode == "Marker") {
-                context.fillStyle = bg.value;
-                context.fillRect(event.offsetX - brush_width / 2, event.offsetY - brush_width / 2, brush_width, brush_width);
+                context.clearRect(event.offsetX - brush_width / 2, event.offsetY - brush_width / 2, brush_width, brush_width);
             }
         };
         // Режим маркера
@@ -55,8 +61,7 @@ if (display == 'Computer') {
         }
         // Режим ластика
         else if (mode == "Marker") {
-            context.fillStyle = bg.value;
-            context.fillRect(e.offsetX - brush_width / 2, e.offsetY - brush_width / 2, brush_width, brush_width);
+            context.clearRect(e.offsetX - brush_width / 2, e.offsetY - brush_width / 2, brush_width, brush_width);
         }
         canvas.onmouseup = () => {
             canvas.onmousemove = null;
@@ -79,8 +84,7 @@ else if (display == 'Mobile') {
         }
         // Режим ластика
         else if (mode == "Marker") {
-            context.fillStyle = bg.value;
-            context.fillRect(e.touches[0].pageX - canvas.getBoundingClientRect().left - brush_width / 2, e.touches[0].pageY - canvas.getBoundingClientRect().top - brush_width / 2, brush_width, brush_width);
+            context.clearRect(e.touches[0].pageX - canvas.getBoundingClientRect().left - brush_width / 2, e.touches[0].pageY - canvas.getBoundingClientRect().top - brush_width / 2, brush_width, brush_width);
 
         }
         canvas.addEventListener('touchmove', canvas.touchmove = (event) => {
@@ -91,8 +95,7 @@ else if (display == 'Mobile') {
             }
             // Режим ластика
             else if (mode == "Marker") {
-                context.fillStyle = bg.value;
-                context.fillRect(event.touches[0].pageX - canvas.getBoundingClientRect().left - brush_width / 2, event.touches[0].pageY - canvas.getBoundingClientRect().top - brush_width / 2, brush_width, brush_width);
+                context.clearRect(event.touches[0].pageX - canvas.getBoundingClientRect().left - brush_width / 2, event.touches[0].pageY - canvas.getBoundingClientRect().top - brush_width / 2, brush_width, brush_width);
             }
         }, false);
         canvas.addEventListener('touchend', canvas.touchend = () => {
@@ -104,8 +107,6 @@ else if (display == 'Mobile') {
 // Отчистка холста
 function clear() {
     context.clearRect(0, 0, canvas.width, canvas.height);
-    context.fillStyle = bg.value;
-    context.fillRect(0, 0, canvas.width, canvas.height);
 }
 // Меняем режим рисования для понимания пользователя
 function eraser() {
@@ -121,8 +122,8 @@ function eraser() {
 bg.addEventListener('input', bg_color, false);
 
 function bg_color() {
-    context.fillStyle = bg.value;
-    context.fillRect(0, 0, canvas.width, canvas.height);
+    context_bg.fillStyle = bg.value;
+    context_bg.fillRect(0, 0, canvas_bg.width, canvas_bg.height);
 }
 
 // Корректируем значение толщины маркера если оно неположительное
@@ -132,8 +133,16 @@ function width_marker_correction() {
     }
 }
 
-// Сохраняем рисунок
-document.getElementById("save_button").onclick = function () {
-    let image = canvas.toDataURL("image/jpg");
-    this.href = image;
+// Сохраняем рисунок путем переноса заднего фона и самого рисунка на третий холст, который не виден юзеру
+function save(){
+    let canvas_save = document.getElementById("canvas_save");
+    let context_save = canvas_save.getContext("2d");
+    canvas_save.width = canvas_save.clientWidth;
+    canvas_save.height = canvas_save.clientHeight;
+    // Переносим на холст сохранения холст заднего фона
+    context_save.drawImage(canvas_bg, 0, 0, canvas.width, canvas.height);
+    // Переносим на холст сохранения холст рисования
+    context_save.drawImage(canvas, 0, 0, canvas.width, canvas.height);
+    let image = canvas_save.toDataURL("image/jpg");
+    document.getElementById("save_button").href = image;
 }
